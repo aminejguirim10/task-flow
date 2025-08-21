cd86b049e512a169120bc0b42ed85d2a556ea0f0
aminejguirim10 <aminejguirim10@gmail.com>
2025-08-21 21:37:32 +0100

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/sonner"
+import VercelAnalytics from "@/components/layout/vercel-analytics"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <VercelAnalytics />
       </body>
     </html>
   )
